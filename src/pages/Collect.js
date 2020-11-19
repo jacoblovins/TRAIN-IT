b07197589a56e9ab4c1cccec929f43564f6c5a07
jacoblovins <lovins.jacob@yahoo.com>
2020-11-19 08:32:22 -0500

    refactored 1

--- src/pages/Collect.js
+++ src/pages/Collect.js
@@ -9,80 +9,70 @@ let inputs;
 let brain;
 let classify = false;
 let finalGesture = "";
-// const classifySpeed = 300;
 const recordTime = 7;
-let recordReady = false;
-const gestureLabels = [];
+
 
 
 function Collect() {
 
-    const [recordCount, setRecordCount] = useState(0)
-    const [recordStatus, setRecordStatus] = useState("  ")
-    const [loadingStatus, setLoadingStatus] = useState("Please Wait! Loading...")
-    const [loading, setLoading] = useState(true)
+    const [recordCount, setRecordCount] = useState(0);
+    const [recordStatus, setRecordStatus] = useState("");
+    const [loadingStatus, setLoadingStatus] = useState("Please Wait! Loading...");
+    const [loading, setLoading] = useState(true);
+    const [recordReady, setRecordReady] = useState(false);
+    const [gestureLabels, setGestureLabels] = useState([]);
     const inputEl = useRef(null);
     const webcamRef = useRef(null);
-
-    const poseParameters = {
-        webcamWidth: 0,
-        webcamHeight: 0,
-        videoHidden: false,
-    };
 
     const init = async () => {
         const video = webcamRef.current.video;
         const videoWidth = video.videoWidth;
         const videoHeight = video.videoHeight;
-
         video.width = videoWidth;
         video.height = videoHeight;
-        console.log(video.width);
 
         let options = {
-            inputs: 42,
+            inputs: 63,
             outputs: recordCount,
             task: 'classification',
             debug: true
         }
-
         brain = ml5.neuralNetwork(options);
 
         const handpose = ml5.handpose(video, modelLoaded);
         function modelLoaded() {
             console.log('HandPose Model Loaded!');
-            setLoadingStatus("Step 1: Name and record at least 2 hand gestures!")
-            poseParameters.webcamWidth = 640;
-            poseParameters.webcamHeight = 480;
+            setLoadingStatus("Step 1: Name and record at least 2 hand gestures!");
             setLoading(false);
         }
         handpose.on('predict', detect);
     }
 
     useEffect(() => {
-        init()
+        init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
 
     const detect = (poses) => {
         if (poses.length > 0) {
-            recordReady = true
+            setRecordReady(true);
             if (collecting || classify) {
                 inputs = [];
                 for (let i = 0; i < poses[0].landmarks.length; i++) {
                     inputs.push(poses[0].landmarks[i][0]);
                     inputs.push(poses[0].landmarks[i][1]);
+                    inputs.push(poses[0].landmarks[i][2]);
                 }
             }
             if (collecting) {
-                console.log(inputs);
                 let target = [targetLabel];
                 brain.addData(inputs, target);
             } else if (classify) {
-                brain.classify(inputs, gotResult)
+                brain.classify(inputs, gotResult);
             }
         } else {
-            recordReady = false
+            setRecordReady(false);
         }
     }
 
@@ -91,31 +81,29 @@ function Collect() {
     // ==============================================================================================
 
     function collect() {
-        console.log(targetLabel);
         collecting = true;
         console.log("Collecting Data");
         setTimeout(() => {
             collecting = false;
             if (recordCount >= 1) {
-                setLoadingStatus("Step 2: Click train and watch the Neural network learn!")
+                setLoadingStatus("Step 2: Click train and watch the Neural network learn!");
             }
-            setRecordStatus("")
+            setRecordStatus("");
+            console.log("Finished Collecting");
             if (!gestureLabels.includes(targetLabel)) {
-                setRecordCount(recordCount + 1)
+                setRecordCount(recordCount + 1);
             }
-            gestureLabels.push(targetLabel)
+            setGestureLabels([...gestureLabels, targetLabel]);
         }, recordTime * 1000)
     }
 
     function handleRecord() {
         if (!recordReady) {
-            alert("Please place your hand in view of the camera before recording")
+            alert("Please place your hand in view of the camera before recording");
         } else {
-            // setRecordCount(recordCount + 1)
-            targetLabel = inputEl.current.value
-            // gestureLabels.push(targetLabel)
-            setRecordStatus("Recording")
-            collect()
+            targetLabel = inputEl.current.value;
+            setRecordStatus("Recording");
+            collect();
         }
     }
 
@@ -129,7 +117,7 @@ function Collect() {
             brain.normalizeData();
             brain.train({ epochs: 30 }, () => {
                 console.log("model trained");
-                setLoadingStatus("Step 3: Click the classify button to try it out or click save to download your trained files!");
+                setLoadingStatus("Step 3: Click the Try It Out button and watch it guess or click save to download your trained files!");
             });
         }
     }
@@ -139,35 +127,27 @@ function Collect() {
     // CLASSIFY
     // ==============================================================================================
 
-    let inter;
-
     function startClass() {
-        classify = true
-        displayClass()
+        classify = true;
+        displayClass();
     }
 
     function displayClass() {
         setInterval(() => {
             setRecordStatus(finalGesture);
-        }, 300)
-    }
-
-    function stopClass() {
-        classify = false;
-        finalGesture = "";
+        }, 300);
     }
 
     function gotResult(error, results) {
-        if (results[0].confidence > 0.87) {
+        if (results[0].confidence > 0.92) {
             console.log(results[0].label);
             finalGesture = results[0].label;
-
         }
     }
 
 
     async function handleSave() {
-        brain.save()
+        brain.save();
     }
 
     return (
